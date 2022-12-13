3347a9ce8679e5098b602e7ac62086a7f8c768e8
Tran Viet An <vietantran1215@gmail.com>
2022-12-14 00:33:26 +0700

    FAdditional hiding, fixbug for queryDOM

--- block-suggestions.js
+++ block-suggestions.js
@@ -1,7 +1,23 @@
 (() => {
-  setTimeout(() => {
-    document.querySelector(
-      "#page-manager > ytd-browse > ytd-two-column-browse-results-renderer"
-    ).style.display = "none";
-  }, 100);
+  function retryDom() {
+    const selectors = {
+      suggestions:
+        "#page-manager > ytd-browse > ytd-two-column-browse-results-renderer",
+      shortVideos: "#items > ytd-guide-entry-renderer:nth-child(2)",
+      subscriptionPage: "#items > ytd-guide-entry-renderer:nth-child(3)",
+      subscriptionSection:
+        "#sections > ytd-guide-section-renderer:nth-child(2)",
+      exploreSection: "#sections > ytd-guide-section-renderer:nth-child(3)",
+      moreOnYoutube: "#sections > ytd-guide-section-renderer:nth-child(4)",
+    };
+    const domCollections = Object.keys(selectors).map(key => document.querySelector(selectors[key]));
+    if (domCollections.some(dom => dom === null)) {
+      retryDom()
+    } else {
+      domCollections.forEach(dom => dom.style.display = 'none');
+    }
+  }
+  window.addEventListener("load", function () {
+    retryDom();
+  });
 })();
