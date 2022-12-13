9bdd95a890ac2b3786c63f4895a2cf35d1e70406
Tran Viet An <vietantran1215@gmail.com>
2022-12-13 23:05:50 +0700

    Done block suggestion

---
+++ block-suggestions.js
@@ -0,0 +1,7 @@
+(() => {
+  setTimeout(() => {
+    document.querySelector(
+      "#page-manager > ytd-browse > ytd-two-column-browse-results-renderer"
+    ).style.display = "none";
+  }, 100);
+})();
